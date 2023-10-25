4a8b17387d16e3ff7c51a873ec6aa782437d72f0
Timo <timoengelvaart@live.nl>
2023-10-25 22:40:34 +0200

    Initial Commit

---
+++ BandPal/BandPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  BandPalApp.swift
+//  BandPal
+//
+//  Created by Timo Engelvaart on 25/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct BandPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
