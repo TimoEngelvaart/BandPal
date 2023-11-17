f10b47694387a1be7ef52a8e11bf2ff8d1992996
Timo <timoengelvaart@live.nl>
2023-11-17 19:31:56 +0100

    improve BottomBorderView code

--- BandPal/Views/Shared/BottomBorderView.swift
+++ BandPal/Views/Shared/BottomBorderView.swift
@@ -1,89 +1,52 @@
 import SwiftUI
 
 struct BottomBorderView: View {
+    // Example item names, adjust as needed
+    private let items = ["Home", "Profile", "Settings"]
+
     var body: some View {
         VStack(alignment: .center, spacing: 0) {
-            VStack(alignment: .center, spacing: 0) {
-                
-                HStack(alignment: .center, spacing: 20) {
-                    //Icon + Text
-                    VStack(alignment: .center, spacing: 2) {
-                        HStack(alignment: .center, spacing: 0) {
-                            Image("Home")
-                            .frame(width: 19, height: 20)
-                        }
-                        .padding(.horizontal, 2.5)
-                        .padding(.vertical, 2)
-                        .frame(width: 24, height: 24, alignment: .center)
-                        Text("Home")
-                          .font(
-                            Font.custom("Urbanist", size: 10)
-                              .weight(.medium)
-                          )
-                          .kerning(0.2)
-                          .multilineTextAlignment(.center)
-                          .foregroundColor(Color(red: 0.62, green: 0.62, blue: 0.62))
-                          .frame(maxWidth: .infinity, alignment: .center)
+            HStack(alignment: .center, spacing: 20) {
+                ForEach(items, id: \.self) { item in
+                    Button(action: {
+                        // Handle item click here
+                        print("\(item) clicked")
+                    }) {
+                        ItemView(name: item)
                     }
-                    VStack(alignment: .center, spacing: 2) {
-                        HStack(alignment: .center, spacing: 0) {
-                            Image("Home")
-                            .frame(width: 19, height: 20)
-                        }
-                        .padding(.horizontal, 2.5)
-                        .padding(.vertical, 2)
-                        .frame(width: 24, height: 24, alignment: .center)
-                        Text("Home")
-                          .font(
-                            Font.custom("Urbanist", size: 10)
-                              .weight(.medium)
-                          )
-                          .kerning(0.2)
-                          .multilineTextAlignment(.center)
-                          .foregroundColor(Color(red: 0.62, green: 0.62, blue: 0.62))
-                          .frame(maxWidth: .infinity, alignment: .center)
-                    }
-                    VStack(alignment: .center, spacing: 2) {
-                        HStack(alignment: .center, spacing: 0) {
-                            Image("Home")
-                            .frame(width: 19, height: 20)
-                        }
-                        .padding(.horizontal, 2.5)
-                        .padding(.vertical, 2)
-                        .frame(width: 24, height: 24, alignment: .center)
-                        Text("Home")
-                          .font(
-                            Font.custom("Urbanist", size: 10)
-                              .weight(.medium)
-                          )
-                          .kerning(0.2)
-                          .multilineTextAlignment(.center)
-                          .foregroundColor(Color(red: 0.62, green: 0.62, blue: 0.62))
-                          .frame(maxWidth: .infinity, alignment: .center)
-                    }
-                    
-                    .padding(0)
-                .frame(maxWidth: .infinity, alignment: .top)
                 }
-                .padding(.horizontal, 32)
-                .padding(.vertical, 0)
-                .frame(maxWidth: .infinity, minHeight: 48, maxHeight: 48, alignment: .center)
             }
-            .padding(.horizontal, 0)
+            .padding(.horizontal, 32)
+            .frame(maxWidth: .infinity, minHeight: 48)
             .padding(.top, 8)
-            .padding(.bottom, 0)
-            .frame(width: 428, alignment: .top)
-        
-            .cornerRadius(24)
         }
-        .padding(.horizontal, 0)
-        .padding(.top, 8)
-        .padding(.bottom, 0)
-        .frame(width: 428, alignment: .top)
+        .frame(width: 428)
         .cornerRadius(24)
     }
 }
 
-#Preview {
-    BottomBorderView()
+struct ItemView: View {
+    let name: String
+
+    var body: some View {
+        VStack(alignment: .center, spacing: 2) {
+            Image(name) // Use your image names
+                .frame(width: 19, height: 20)
+                .padding(.horizontal, 2.5)
+                .padding(.vertical, 2)
+                .frame(width: 24, height: 24)
+            Text(name)
+                .font(Font.custom("Urbanist", size: 10).weight(.medium))
+                .kerning(0.2)
+                .multilineTextAlignment(.center)
+                .foregroundColor(Color(red: 0.62, green: 0.62, blue: 0.62))
+                .frame(maxWidth: .infinity)
+        }
+    }
+}
+
+struct BottomBorderView_Previews: PreviewProvider {
+    static var previews: some View {
+        BottomBorderView()
+    }
 }
